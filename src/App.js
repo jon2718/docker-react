fc09f544fd289fdec58aa03901c54808fa19f3a1
Jon Lederman <jonlederman@ZELDAs-iMac-2.local>
2020-04-29 10:55:06 -0400

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.  Thank you so much...for everything.  Good day!!!
+          Edit <code>src/App.js</code> and save to reload.  Thank you so much...for everything.  Good day and night!!!
         </p>
         <a
           className="App-link"
